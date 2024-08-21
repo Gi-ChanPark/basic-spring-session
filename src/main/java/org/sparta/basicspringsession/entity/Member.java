52fc75a0c75f5faab6751c4234cb4da5281ea27b
Gi-ChanPark <parkland0859@gmail.com>
2024-08-21 17:34:03 +0900

    수정

--- src/main/java/org/sparta/basicspringsession/entity/Member.java
+++ src/main/java/org/sparta/basicspringsession/entity/Member.java
@@ -3,6 +3,7 @@ package org.sparta.basicspringsession.entity;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
